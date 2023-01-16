d49a948c5953c57a24d56eb9660fb5824bdad330
Caleb A. Bostic-Gardner <13528490+CalebABG@users.noreply.github.com>
2023-01-16 00:46:47 -0500

    Update `CreateJournalForm` component to add dropdown select for Category

--- src/SoarBeyond.Web/Components/Journal/CreateJournalForm.razor.cs
+++ src/SoarBeyond.Web/Components/Journal/CreateJournalForm.razor.cs
@@ -1,19 +1,28 @@
 ﻿using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Components;
 using SoarBeyond.Domain.Dto;
+using SoarBeyond.Domain.Requests.Categories;
 
 namespace SoarBeyond.Web.Components;
 
 public partial class CreateJournalForm
 {
+    [Inject] public IMediator Mediator { get; set; }
     [Parameter] public bool ShowTitle { get; set; } = true;
     [Parameter] public EventCallback<Journal> OnValidSubmit { get; set; }
 
     private Journal _journal = new();
+    private List<Category> _categories;
 
     public CreateJournalForm()
     {
         Class = "card shadow";
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        _categories = (await Mediator.Send(new GetAllCategoriesRequest(await GetUserIdAsync()))).ToList();
     }
 
     private async Task HandleValidSubmitAsync()
@@ -31,5 +40,11 @@ public partial class CreateJournalForm
     public void ResetForm()
     {
         ResetModel();
+    }
+
+    private void HandleCategorySelected(ChangeEventArgs args)
+    {
+        int value = int.Parse(args.Value!.ToString()!);
+        _journal.CategoryId = value < 1 ? null : value;
     }
 }
